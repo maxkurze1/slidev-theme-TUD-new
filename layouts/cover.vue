4c56287b080926d54d23cb3f5a8040be1a058319
Max Kurze <Max.Kurze@barkhauseninstitut.org>
2026-07-11 13:43:39 +0200

    replace sym-links

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,1 +1,10 @@
-cover-blue.vue
+<!-- Alias: for the `cover-blue` layout. -->
+<template>
+  <CoverBlue>
+    <slot />
+  </CoverBlue>
+</template>
+
+<script setup lang="ts">
+import CoverBlue from './cover-blue.vue'
+</script>
--- layouts/section.vue
+++ layouts/section.vue
@@ -1,1 +1,10 @@
-section-blue.vue
+<!-- Alias: for the `section-blue` layout. -->
+<template>
+  <SectionBlue>
+    <slot />
+  </SectionBlue>
+</template>
+
+<script setup lang="ts">
+import SectionBlue from './section-blue.vue'
+</script>
